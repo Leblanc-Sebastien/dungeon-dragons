093dd4b3d6ef0f7ad08c67cd2c56a1ddf2f36af4
sugiilol <sugii.noob@gmail.com>
2023-06-09 15:31:37 +0200

    display Form component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-
+import Form from "./Components/Form/Form"
 function App() {
-
-  const [counterState, setCounterState] = useState(0)
-  const [msgState, setMsgState] = useState("")
-
-  const incrementCounter = () => {
-    setCounterState(counterState + 1)
-  }
-
-  useEffect(() => {
-    if(counterState > 5){
-      setMsgState("Superieur à 5")
-    }
-    else if(counterState < 5){
-      setMsgState("inferieur à 5")
-    }
-    else{
-      setMsgState("")
-    }
-  }, [counterState])
-  
 
   return (
     <div className="App">
-        <button onClick={incrementCounter}>clik enculé {counterState}</button>     
-           <div>{msgState}</div>
+        <Form />
     </div>
   );
 }
