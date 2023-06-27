6d89536619559ca960e2305597e216321f47b12c
sugiilol <sugii.noob@gmail.com>
2023-06-27 18:20:20 +0200

    fields in the character creation form added

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -5,13 +5,25 @@ import "./Form.css"
 
 export default function Form() {
 
-    const [createdCaracters, setCreatedCaraters] = useState([
-        { id: uuidv4(), pseudo: "SuGii", classe: "Guerrier", age: "36" },
-        { id: uuidv4(), pseudo: "Michel", classe: "Mage", age: "67" },
-        { id: uuidv4(), pseudo: "Gimli", classe: "Tueur de troll", age: "159" },
-        { id: uuidv4(), pseudo: "Gandoulfi", classe: "Mage des profondeurs des anus", age: "19867" },
-    ])
+    // const [createdCaracters, setCreatedCaraters] = useState([
+    //     { id: uuidv4(), pseudo: "SuGii", classe: "Guerrier", age: "36" }
+    // ])
 
+    const [createdCaracters, setCreatedCaraters] = useState([
+        {
+            id: uuidv4(),
+            date: "16/09/1987", 
+            nom: "SuGii", 
+            race: "Nain",
+            classe: "Guerrier", 
+            carriere: "Tueur de troll",
+            age: "36", 
+            taille: "113",
+            cheveux: "Chatain",
+            yeux: "bleu"
+        }
+    ])
+    
     const [newPseudoCaracterInput, setNewPseudoCaracterInput] = useState("")
     const [newClassCaracterInput, setNewClassCaracterInput] = useState("")
     const [newAgeCaracterInput, setNewAgeCaracterInput] = useState("")
@@ -32,11 +44,19 @@ export default function Form() {
     const addNewCaracter = (e) => {
         e.preventDefault()
         if (newPseudoCaracterInput !== "" && newClassCaracterInput !== "" && newAgeCaracterInput !== "") {
+
+            const toDay = new Date()
             const NewCaracter = {
                 id: uuidv4(),
-                pseudo: newPseudoCaracterInput,
+                date: toDay.getDate() +"/"+ (toDay.getMonth() + 1) +"/"+ toDay.getFullYear(),
+                nom: newPseudoCaracterInput,
+                race: "??",
                 classe: newClassCaracterInput,
-                age: newAgeCaracterInput
+                carriere: "Tueur de troll",
+                age: newAgeCaracterInput, 
+                taille: "113",
+                cheveux: "Chatain",
+                yeux: "bleu"
             }
 
             const newArrCaracter = [...createdCaracters]
@@ -67,18 +87,48 @@ export default function Form() {
                 <div className="popup-content">
                     <h2>Création de personnage</h2>
                     <form className="form-create-character" onSubmit={e => addNewCaracter(e)}>
-                        <label className="label-create-character">
-                            Pseudo :
-                            <input className="input-create-character" type="text" name="pseudo" value={newPseudoCaracterInput} onInput={e => setPseudo(e.target.value)} />
-                        </label>
-                        <label className="label-create-character">
-                            classe :
-                            <input className="input-create-character" type="text" name="classe" value={newClassCaracterInput} onInput={e => setClass(e.target.value)} />
-                        </label>
-                        <label className="label-create-character">
-                            age :
-                            <input className="input-create-character" type="text" name="age" value={newAgeCaracterInput} onInput={e => setAge(e.target.value)} />
-                        </label>
+                        <label className="label-create-character">Nom :</label>                           
+                        <input className="input-create-character" type="text" name="pseudo" value={newPseudoCaracterInput} onInput={e => setPseudo(e.target.value)} />                        
+                        <label className="label-create-character">Race :</label>                           
+                        <select name="race" id="race-select">
+                            <option value="">Choisis une race</option>
+                            <option value="humain">Humain</option>
+                            <option value="nain">Nain</option>
+                            <option value="elfe">Elfe</option>
+                            <option value="halfeling">Halfeling</option>
+                        </select> 
+                        <label className="label-create-character">Classe :</label>  
+                        <select name="classe" id="classe-select">
+                            <option value="">Choisis une classe</option>
+                            <option value="guerrier">Guerrier</option>
+                            <option value="mage">Mage</option>
+                            <option value="voleur">Voleur</option>
+                            <option value="druide">Druide</option>
+                        </select> 
+                        <label className="label-create-character">Carrière :</label> 
+                        <input className="input-create-character" type="text" name="carriere"/>                                                               
+                        <label className="label-create-character">age :</label>  
+                        <input className="input-create-character" type="number" name="age" value={newAgeCaracterInput} onInput={e => setAge(e.target.value)} />  
+                        <label className="label-create-character">Taille :</label>     
+                        <input className="input-create-character" type="number" />   
+                        <label className="label-create-character">Cheveux :</label>  
+                        <select name="cheveux" id="cheveux-select">
+                            <option value="">Choisis une couleur</option>
+                            <option value="blond">Blond</option>
+                            <option value="brun">Brun</option>
+                            <option value="chatain">Chatain</option>
+                            <option value="roux">Roux</option>
+                            <option value="lgbtq+">LGBTQ+</option>
+                        </select> 
+                        <label className="input-create-character">Yeux</label>
+                        <select name="yeux" id="yeux-select">
+                            <option value="">Choisis une couleur</option>
+                            <option value="marron">Marron</option>
+                            <option value="bleu">Bleu</option>
+                            <option value="vert">Vert</option>
+                            <option value="rouge">Rouge</option>
+                            <option value="lgbtq+">LGBTQ+</option>
+                        </select>                                                     
                         <button className="btn-validate">Valider</button>
                     </form>
                     <a href="#" className="close-popup">X</a>
--- src/Components/TableCharacters/TableCharacters.js
+++ src/Components/TableCharacters/TableCharacters.js
@@ -5,17 +5,29 @@ export default function TableCharacters(props) {
     return (
         <table className="table-caracter">
             <thead>
-                <th className="th-caracter" scope='col'>Pseudo</th>
+                <th className="th-caracter" scope='col'>Date</th>
+                <th className="th-caracter" scope='col'>Nom</th>
+                <th className="th-caracter" scope='col'>Race</th>
                 <th className="th-caracter" scope='col'>Classe</th>
+                <th className="th-caracter" scope='col'>Carriere</th>
                 <th className="th-caracter" scope='col'>Age</th>
+                <th className="th-caracter" scope='col'>Taille</th>
+                <th className="th-caracter" scope='col'>Cheveux</th>
+                <th className="th-caracter" scope='col'>Yeux</th>
             </thead>
             <tbody>
                 {props.charactersList.map((caracter) => {
                     return (
-                        <tr>
-                            <td>{caracter.pseudo}</td>
+                        <tr> 
+                            <td>{caracter.date}</td>   
+                            <td>{caracter.nom}</td>
+                            <td>{caracter.race}</td>
                             <td>{caracter.classe}</td>
+                            <td>{caracter.carriere}</td>
                             <td>{caracter.age}</td>
+                            <td>{caracter.taille} cm</td>
+                            <td>{caracter.cheveux}</td>
+                            <td>{caracter.yeux}</td>
                             <td><button className='btn-delete-table' onClick={() => (props.deleteCaracter(caracter.id))}> Del </button></td>
                             <td><button className='btn-plus-table'> ... </button></td>
                         </tr>
