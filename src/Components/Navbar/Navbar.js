e8808ff16e095d67340e3397fce21445d15bf3ae
sugiilol <sugii.noob@gmail.com>
2023-07-06 19:04:01 +0200

    navbar added part 2

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,16 +1,17 @@
 import "./Navbar.css"
+import imgSwords from "./img/double-sword.png"
 
 
 export default function Navbar() {
 
     return (
         <nav className="navbar">
-            <a href="#"><img src="./img/double-sword.png" alt="double sword" /></a>
+            <a href="#"><img className="img-double-sword" src={imgSwords} alt="double sword" /></a>
             <ul className="navbar-link">
-                <li><a href="/"></a>Lien 1</li>
-                <li><a href="#"></a>Lien 2</li>
-                <li><a href="#"></a>Lien 3</li>
-
+                <li><a href="/">Home</a></li>
+                <li><a href="#">Création de personnage</a></li>
+                <li><a href="#">Infos</a></li>
+                <li><a href="#">Contact</a></li>
             </ul>
         </nav>
     )
