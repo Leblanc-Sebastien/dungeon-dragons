2869a4972cd002b3aba74dc029f7e64f15ff8211
sugiilol <sugii.noob@gmail.com>
2023-07-21 18:59:24 +0200

    css new form

--- src/Components/ReactHookForm/ReactHookForm.js
+++ src/Components/ReactHookForm/ReactHookForm.js
@@ -5,6 +5,8 @@ import "./ReactHookForm.css"
 
 export default function ReactHookForm() {
 
+    const [raceState, setRaceState] = useState("")
+
     const [characters, setCharacters] = useState([
         {
             id: uuidv4(),
@@ -50,16 +52,24 @@ export default function ReactHookForm() {
         console.log(characters)
     }
 
+    const race = (e) => {
+        setRaceState(e)
+        JSON.stringify(console.log(raceState))     
+    }
+
     return (   
         <form className="wrapper" onSubmit={handleSubmit(onSubmit)}>
+            <label>Nom</label>
             <input defaultValue={"Michel"}{...register("name", { required: true, maxLength: 10, minLength: 2 })} />           
-            <select {...register("race", { required: true })}>
+            <label>Race</label>
+            <select {...register("race", { required: true })} onChange={e => race(e.target.value)}>
                 {/* <option value="">Choisis une race</option> */}
                 <option value="humain">Humain</option>
                 <option value="nain">Nain</option>
                 <option value="elfe">Elfe</option>
                 <option value="halfeling">Halfeling</option>
             </select>
+            <label>Classe</label>
             <select {...register("class", { required: true })}>
                 {/* <option value="">Choisis une classe</option> */}
                 <option value="guerrier">Guerrier</option>
@@ -67,12 +77,22 @@ export default function ReactHookForm() {
                 <option value="voleur">Voleur</option>
                 <option value="druide">Druide</option>
             </select>
+            <label>Carrière</label>
             <input defaultValue={"Tueur de troll"}{...register("career", {required: true, maxLength:20, minLength: 2})} />
+            <label>Echelon</label>
             <input defaultValue={"6"}{...register("echelon", {required: true, maxLength:20, minLength: 1})} />
+            <label>Plan de carrière</label>
             <input defaultValue={"Tueur de troll supérieur"}{...register("careerPlan", {required: true, minLength: 2})} />
-            <input defaultValue={"vivant"}{...register("statut", {required: true, maxLength:20, minLength: 2})} />
+            <label>Statut</label>
+            <select {...register("statut", {required: true})}>
+                <option value="alive">Vivant</option>
+                <option value="dead">Mort</option>
+            </select>
+            <label>Age</label>
             <input defaultValue={36} type="number" {...register("age", { required: true, maxLength: 4, minLength: 1 })} />
+            <label>Taille</label>
             <input defaultValue={123} type="number" {...register("height", { required: true, maxLength: 3, minLength: 1 })} />
+            <label>Cheveux</label>
             <select {...register("hair", { required: true })}>
                 {/* <option value="">Choisis une couleur de cheveux</option> */}
                 <option value="brun">Brun</option>
@@ -81,6 +101,7 @@ export default function ReactHookForm() {
                 <option value="chatain">Chatain</option>
                 <option value="lgbt+">LGBT+</option>
             </select>
+            <label>Yeux</label>
             <select {...register("eyes", { required: true })}>
                 {/* <option value="">Choisis une couleur d'yeux</option> */}
                 <option value="noir">Noir</option>
@@ -91,6 +112,11 @@ export default function ReactHookForm() {
                 <option value="lgbt+">LGBT+</option>
             </select>
             <button type="submit">Validage</button>
+            {raceState === "nain" ?(
+                <p>Je suis un NAIN</p>
+            ) : (
+                <p>Je suis rien</p>
+            )}
         </form>      
     )
 }
