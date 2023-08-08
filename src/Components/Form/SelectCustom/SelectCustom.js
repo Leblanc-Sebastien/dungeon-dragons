372db32f46e36386886838919ec2ce72daf9c645
SuGii <sugii.noob@gmail.com>
2023-08-08 08:29:53 +0200

    validation test added  on select custom

--- src/Components/Form/SelectCustom/SelectCustom.js
+++ src/Components/Form/SelectCustom/SelectCustom.js
@@ -2,23 +2,27 @@ import { useEffect, useState } from 'react'
 import './SelectCustom.css'
 import { v4 as uuidv4 } from 'uuid';
 
-export default function SelectCustom(props){
+export default function SelectCustom(props) {
 
     const [itemState, setItemState] = useState('')
 
     useEffect(() => {
-        props.onItemChange(itemState)
-    },[itemState])
-    
-    return(
-        <>  
-        <label>{props.itemLabel}</label>
+        const validationTest = props.itemsList.filter((item) => item.name === itemState).length
+        console.log(validationTest, itemState)
+        if (validationTest !== 0) {
+            props.onItemChange(itemState)
+        }
+    }, [itemState])
+
+    return (
+        <>
+            <label>{props.itemLabel}</label>
             <select name={props.itemLabel} id={uuidv4()} onChange={e => setItemState(e.target.value)}>
-                <option value={props.itemLabel}>choisir une option</option>
-                {props.itemsList.map((race)=>{
-                    return(
-                       <option key={race.id} value={race.name} >{race.name}</option> 
-                    )               
+                <option value={props.itemLabel}>Choisir une {props.itemLabel}</option>
+                {props.itemsList.map((race) => {
+                    return (
+                        <option key={race.id} value={race.name} >{race.name}</option>
+                    )
                 })}
             </select>
         </>
